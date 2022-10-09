b7c524cff6fc244a3bda4a6aeb62ba3b852bacda
Jonathan Martin <jonathan.h.martin@protonmail.com>
2022-10-09 16:50:52 -0400

    adding tables and queries from online learing and challenge from Module 7. Modified schema and queries.

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,55 @@
+-- The Number of Retiring Employees by Title
+SELECT e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles.csv
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+
+-- The Number of unique titles 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO unique_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (ti.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.to_date DESC;
+
+-- Retrieve the number of employees by their most recent job title that are about to retire
+SELECT COUNT(ut.title), 
+	ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
+
+-- Create a Mentorship Eligibility for eligible employees
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (ti.to_date = '9999-01-01')
+    AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+
+    
--- Queries/schema.sql
+++ Queries/schema.sql
@@ -40,12 +40,13 @@ CREATE TABLE salaries (
 
 --Creating Department Employees table
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 --Creating Titles table
@@ -54,8 +55,8 @@ CREATE TABLE titles (
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, title, from_date),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 --Confirming the tables were created successfully
